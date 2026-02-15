2aa2cb997f827bdb1f6234087fb52f06d8d062e9
aaaakshatttt <veeakshat4@gmail.com>
2026-02-15 09:09:18 +0530

    i tried

--- src/array.java
+++ src/array.java
@@ -1,5 +1,9 @@
 public class array {
     public static void main(String[] args) {
+        int[][] arr = {{25, 30 , 29} , {22, 22, 25}};
+        // 25 is position 0 and 30 is position 1 ans sp pn
+        System.out.println(arr[1][0]);
+        //first strong bracket is the 2nd array and 2nd strong bracjey has the position
 
     }
 }
