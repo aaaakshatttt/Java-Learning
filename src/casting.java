9b14dadd06fac9f777cdb861ccf4179bef3898e0
aaaakshatttt <veeakshat4@gmail.com>
2026-02-15 09:03:06 +0530

    i tried

---
+++ src/casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    public static void main(String[] args) {
+        double money = 19.99;
+        int tax = 5;
+        int add = tax + (int)money;
+        System.out.println(add);
+
+    }
+}
---
+++ src/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        int age = 21;
+        char fav = 'k';
+        String ou6 = "My Age Is ";
+        System.out.println(ou6 + age  + " and my initial is " + fav);
+
+
+    }
+}
