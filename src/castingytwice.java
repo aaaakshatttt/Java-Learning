576fafdf7b87caf2dad367355a077db325142280
aaaakshatttt <veeakshat4@gmail.com>
2026-02-15 09:54:47 +0530

    i tried

--- src/castingytwice.java
+++ src/castingytwice.java
@@ -1,5 +1,9 @@
 public class castingytwice {
     public static void main(String[] args) {
+        double monthlysalary = 5500.75;
+        double total = monthlysalary + 500;
+        int finalcheck = (int)total;
+        System.out.println(finalcheck);
 
 
     }
