f757f8058db338ff4ce7c33ff8b221865d00ee39
aaaakshatttt <veeakshat4@gmail.com>
2026-02-15 10:17:10 +0530

    i tried

---
+++ src/endsessionday1.java
@@ -0,0 +1,17 @@
+public class endsessionday1 {
+    public static void main(String[] args) {
+        /*String name1 = "Akshat";
+        String name2 = "Kashi";
+        String name3 = "Gemini";*/
+
+        String[] arr = {"Kashi", "Akshat", "Gemini"};
+        System.out.println(arr[1].length());
+
+        boolean islongname = arr[2].length() > 5;
+        System.out.println("name is "+ arr[2] + "length is " + arr[2].length() + " long ass name " + islongname);
+
+
+
+
+    }
+}
