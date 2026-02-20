f1825f446c36dd879bd9b6cc457acfc0d04b25cf
aaaakshatttt <veeakshat4@gmail.com>
2026-02-20 23:44:10 +0530

    i tried

--- src/practice.java
+++ src/practice.java
@@ -5,6 +5,109 @@ public class practice {
         String ou6 = "My Age Is ";
         System.out.println(ou6 + age  + " and my initial is " + fav);
 
+        import java.util.Scanner;
+        public class Main {
+
+
+            public static void main(String[] args) {
+       /* System.out.println("Hello, java!");
+        System.out.println("akshast");
+
+        int a = 88;
+        int v = 6;
+        int c = a*v;
+        System.out.println(c);
+
+        String name1 = "akshat ";
+        String name2 = "jss";
+        String fullname = name1 + name2;
+        System.out.println(fullname); */
+
+        /*String hero = "ironman";
+        System.out.println(hero.length());
+        String villian = "victor von doom";
+        System.out.println(villian.substring(7,15));
+
+        char a = '$';
+        System.out.println(a);*/
+
+        /*int[] arr = new int[5];
+        arr[0] = 98;
+        arr[1] = 66;
+        arr[2] = 87;
+        arr[3] = 67;
+        arr[4] = 55;
+        //int[] arr = {98,66,87,67,55};
+        System.out.println(arr[3]);*/
+
+
+        /*int[] arr = {1,2,3,4,5,6,7,8,9,0};
+        System.out.println(arr[5]);*/
+
+                Scanner sc = new Scanner(System.in);
+/*
+        int[] arr = new int[3];
+        System.out.println("Enter 1st Position in array");
+        arr[0] = sc.nextInt();
+        System.out.println("Enter 2st Position in array");
+        arr[1] = sc.nextInt();
+        System.out.println("Enter 3st Position in array");
+        arr[2] = sc.nextInt();
+
+        System.out.println("1st-" + arr[0] + " 2nd-" + arr[1] + " 3rd-" + arr[2]);
+
+
+        int[][] arr2 = new int[2][2];
+        System.out.println("1st");
+        arr2[0][0] = sc.nextInt();
+        System.out.println("2nd");
+        arr2[0][1] = sc.nextInt();
+        System.out.println("1st");
+        arr2[1][0] = sc.nextInt();
+        System.out.println("2nd");
+        arr2[1][1] = sc.nextInt();
+
+        System.out.println("damn " + arr2[0][0] + " son " + arr2[0][1] + " whaere " + arr2[1][0] + " youuu " + arr2[1][1]); */
+
+         /*
+
+        System.out.println("enter number = ");
+        int a = sc.nextInt();
+        if (a>=10){
+            System.out.println("greater then equal to 10");
+        }
+        else{
+            System.out.println("less then 10");
+        } */
+    /*
+            System.out.println("enter number = ");
+            int num = sc.nextInt();
+            if(num>0){
+                System.out.println("positive number");
+            }
+            else if(num<0){
+                System.out.println("negative number");
+
+            }
+            else {
+                System.out.println("zeroooo");
+            }
+        */
+/*System.out.println("enter number a = ");
+int num1 = sc.nextInt();
+System.out.println("enter 2nd number = ");
+int num2 = sc.nextInt();
+if (num1>num2){
+    System.out.println(num1 + "  is greateer");
+
+}
+else {
+    System.out.println(num2 + " is greater");
+}
+*/
+            }
+        }
+
 
     }
 }
