0564c8516d70b5d0e5ea60d214f9be0a9eb56c20
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 13:40:17 +0530

    i tried

---
+++ src/loop2.java
@@ -0,0 +1,11 @@
+public class loop2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=69; i++){ // 69 baar neeche wala code run hoga matlab 69 lines
+            for (int j = 1;j<6;j++){
+                System.out.print("*  "); //since we got print toh same hi line pe ye 5 baar * print karega
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/looppractice.java
@@ -0,0 +1,5 @@
+public class looppractice {
+    public static void main(String[] args) {
+
+    }
+}
