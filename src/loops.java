9155d934e8dbb74de39655ee5cd6460504de8834
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 12:25:18 +0530

    i tried

--- src/loops.java
+++ src/loops.java
@@ -1,11 +1,13 @@
 public class loops {
     public static void main(String[] args) {
-        for(int i = 1; i <=10;i++){
-            if (i%2!=0) {
-                System.out.println(i);
+        for(int i = 1;i <=10;i++){
+            if(i%2==0){
+                System.out.println("even");
+            } else if (i%2!=0){
+                System.out.println("odd");
             }
 
-            }
+        }
 
         }
 }
