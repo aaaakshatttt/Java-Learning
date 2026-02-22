dba68322cc811120d560e8c2ce5f5b3e2bdf95a3
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 13:31:47 +0530

    i tried

--- src/loops.java
+++ src/loops.java
@@ -1,5 +1,12 @@
 public class loops {
     public static void main(String[] args) {
-        for (int i = 1;i<=5;i++) System.out.println("*****");
+        for (int i = 1; i <= 10; i++) { //kitni baar neeche wala code print karna hai like agar neeche im printing 1 se 9 toh kitni baar 1 se 9 print karna haiiiiii woahhhhhh wtfffffff
+            for (int j = 1; j < +10; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
