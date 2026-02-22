593d36f736b08fda09c7d27eb617340193b65ad7
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 11:49:28 +0530

    i tried

--- src/loops.java
+++ src/loops.java
@@ -1,9 +1,14 @@
 public class loops {
     public static void main(String[] args) {
         for(int i = 1; i <=10;i++){
-            if(i>5){
-                System.out.println(i);
-            }
+            if(i%2==0){
+            System.out.println("*********");
         }
-    }
+            else if (i%2!=0) {
+                System.out.println("   **    ");
+            }
+
+            }
+
+        }
 }
