750a0a7104e23f5f79e283224ac0011ce7209c1f
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 16:04:02 +0530

    i tried

--- src/looppractice.java
+++ src/looppractice.java
@@ -1,5 +1,11 @@
 public class looppractice {
     public static void main(String[] args) {
+        /*for (int i = 3;i>=1;i--){
+            for (int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();*/
+
 
     }
 }
