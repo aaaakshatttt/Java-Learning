ae093b575d2b7fda798a02921bacfdbf83b806ca
aaaakshatttt <veeakshat4@gmail.com>
2026-02-22 12:45:13 +0530

    i tried

--- src/loops.java
+++ src/loops.java
@@ -1,7 +1,5 @@
 public class loops {
     public static void main(String[] args) {
-        for (int i = 1; i<=4;i++){
-            System.out.println("****");
-        }
-        }
+        for (int i = 1;i<=5;i++) System.out.println("*****");
+    }
 }
