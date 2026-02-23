b94f02c49d44bdd46f9f53d387d16c529a19ace0
aaaakshatttt <veeakshat4@gmail.com>
2026-02-23 22:44:01 +0530

    i tried

---
+++ src/prac.java
@@ -0,0 +1,27 @@
+public class prac {
+    public static void main(String[] args) {
+        for (int i = 1;i<=3;i++){
+            for (int j = 1;j<=4;j++){
+            System.out.print("*");
+            }System.out.println();
+        }
+
+        for (int k = 1;k<=10;k++){
+            System.out.print(k + " ");
+
+        }System.out.println();
+
+        for (int ja = 1;ja<=4;ja++){
+            System.out.print("java");
+        }
+        System.out.println();
+
+        for (int e = 1; e<=10;e++){
+            if (e%2==0){
+                System.out.print(e + " ");
+            }
+        }
+
+
+    }
+}
