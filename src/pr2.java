4028a6879a03dff1d9a98abc3a7c3afde83ebbae
aaaakshatttt <veeakshat4@gmail.com>
2026-02-24 23:34:07 +0530

    i tried

--- src/pr2.java
+++ src/pr2.java
@@ -6,12 +6,39 @@ public class pr2 {
             }
             System.out.println();
         }
-*/
+
         for (int i = 1;i <=4;i++){
             for (int j = 1;j <=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        for (int i = 4;i>=1;i--){
+            for (int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }*/
+         for (int i = 1;i<=5;i++){
+             if (i ==1 || i == 5){
+                 for (int j = 1;j<=5;j++){
+                     System.out.print("*");
+                 }
+                 System.out.println();
+             } else if (i == 2|| i == 3|| i == 4) {
+                 for (int k = 1;k<=5;k++){
+                     if (k == 1 || k == 5){
+                         System.out.print("*");
+                     }
+                     else {
+                         System.out.print(" ");
+                     }
+                 }
+                 System.out.println();
+
+             }
+
+         }
     }
 }
