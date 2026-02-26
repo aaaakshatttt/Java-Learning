6ea5623360bbee4ce6eead3c9c3f999188221505
aaaakshatttt <veeakshat4@gmail.com>
2026-02-26 20:54:36 +0530

    i tried

--- src/fxn.java
+++ src/fxn.java
@@ -5,14 +5,37 @@ public class fxn {
         int summing = a + b;
         return summing;
     }
+    public static float damn(float a,float b){
+        float division = a/b;
+        return division;
+    }
+    public static int mul(int a, int b){
+        int mmmmm = a*b;
+        return mmmmm;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter a : ");
+        /*System.out.print("enter a : ");
         int a = sc.nextInt();
         System.out.print("enter b : ");
         int b = sc.nextInt();
         int c = sum(a,b);
-        System.out.println(c);
+        System.out.println(c);*/
+
+
+       /* System.out.print("enter c = ");
+        float c = sc.nextFloat();
+        System.out.print("engter d = ");
+        float d = sc.nextFloat();
+        float divi = damn(c,d);
+        System.out.println(divi); */
+
+        System.out.print("enter e = ");
+        int e = sc.nextInt();
+        System.out.print("enter f =");
+        int f = sc.nextInt();
+        int mnmnm = mul(e,f);
+        System.out.println(mnmnm);
 
     }
 }
