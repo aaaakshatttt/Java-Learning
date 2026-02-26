f8514402a7f498f6b09e08111babc56de84afdc1
aaaakshatttt <veeakshat4@gmail.com>
2026-02-26 23:12:42 +0530

    i tried

--- src/questions.java
+++ src/questions.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class questions {
+    public static float mean(float a,float b,float c){
+        float meean = (a+b+c)/3;
+        return meean;
+    }
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        float a = sc.nextFloat();
+        System.out.println("enter b = ");
+        float b = sc.nextFloat();
+        System.out.println("enter c = ");
+        float c = sc.nextFloat();
+        float z = mean(a,b,c);
+        System.out.println(z);
     }
 }
