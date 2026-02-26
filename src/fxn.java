ad37748e959773f5cece09b864faf1f7f59ff7ca
aaaakshatttt <veeakshat4@gmail.com>
2026-02-26 20:40:56 +0530

    i tried

--- src/fxn.java
+++ src/fxn.java
@@ -1,47 +1,18 @@
+import java.util.Scanner;
+
 public class fxn {
+    public static int sum(int a, int b){
+        int summing = a + b;
+        return summing;
+    }
     public static void main(String[] args) {
-        import java.util.Scanner;
-        class Main {
-            public static int sum(int a,int b){
-                int d = a + b;
-                return d;
-            }
-            public static float multi(float a , float b){
-                float m = a*b;
-                return m;
-            }
-            public static float div(float a, float b){
-                float d = a/b;
-                return d;
-            }
-            public static int remainder(int a, int b){
-                int r = a%b;
-                return r;
-            }
-
-            public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                // System.out.print("enter s: ");
-                //int s = sc.nextInt();
-                // System.out.print("enter f: ");
-                //int f = sc.nextInt();
-        /*int z = sum(s,f);
-        System.out.print(z);*/
-                //float w = sc.nextFloat();
-                // float e = sc.nextFloat();
-                // float are = multi( w, e);
-                // System.out.print(are);
-                //float g = sc.nextFloat();
-                //float b = sc.nextFloat();
-                // float dd = div(g,b);
-                //System.out.print(dd);
-                //nt h = sc.nextInt();
-                //nt j = sc.nextInt();
-                // int rrrrrr = remainder(h,j);
-                // System.out.print(rrrrrr);
-
-            }
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a : ");
+        int a = sc.nextInt();
+        System.out.print("enter b : ");
+        int b = sc.nextInt();
+        int c = sum(a,b);
+        System.out.println(c);
 
     }
 }
