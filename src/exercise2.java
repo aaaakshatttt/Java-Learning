c6197ff94d42d30e88a2022e5be9c46193c8617f
aaaakshatttt <veeakshat4@gmail.com>
2026-03-01 17:15:33 +0530

    i tried

--- src/exercise2.java
+++ src/exercise2.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class exercise2 {
-    public static void main(String[] args) {
-
+    public static void greatest(int a,int b) {
+        if (a > b) {
+            System.out.println(a);
+        }
+        else if (b > a) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(a + " is equal to " + b);
+        }
     }
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int z = sc.nextInt();
+            int y = sc.nextInt();
+            greatest(z,y);
+
+
+        }
+    }
