635214d3ad51f61747f2dbe51fd5410514bbb277
aaaakshatttt <veeakshat4@gmail.com>
2026-03-02 19:08:34 +0530

    i tried

--- src/array.java
+++ src/array.java
@@ -1,21 +1,9 @@
 public class array {
     public static void main(String[] args) {
-        for (int i = 1;i<=4;i++){
-            for (int j = 1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int k = 1;k<=5;k++){
-            System.out.print("*");
-        }
-        System.out.println();
-
-        for (int i = 4;i>=1;i--) {
-            for (int m = 1; m <= i; m++) {
-                System.out.print("*");
-            }        System.out.println();
-
-        }
+        int[] arr = new int[3];
+        arr[0]= 3;
+        arr[1] = 2345;
+        arr[2] = 123;
+        System.out.println(arr[2]);
     }
 }
