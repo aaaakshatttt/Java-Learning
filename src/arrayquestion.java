f3daea189c9dadc5800542e9cc9b369acd9b5709
aaaakshatttt <veeakshat4@gmail.com>
2026-03-04 18:20:19 +0530

    i tried

--- src/arrayquestion.java
+++ src/arrayquestion.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class arrayquestion {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array size = ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        //input
+        for (int i = 0;i<size;i++){
+            System.out.println("enter array position "+ i + " = ");
+            arr[i] = sc.nextInt();
+        }
+
+        //output
+        for (int j = 0;j<size;j++){
+            System.out.println(arr[j]);
+        }
+
+
+
+
+
+
 
     }
 }
