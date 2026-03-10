089672fa5c10f7017f8d201dbc57074e8f3bccb0
aaaakshatttt <veeakshat4@gmail.com>
2026-03-10 23:27:42 +0530

    i tried

--- src/delete.java
+++ src/delete.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 public class delete {
+    public static void square(int n){
+        System.out.println(n*n);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter a = ");
         int a = sc.nextInt();
-        System.out.print("enter b = ");
-        int b = sc.nextInt();
-        if (a>b){
-            System.out.println(a + " is bigger");
-        } else if (b>a) {
-            System.out.println(b + " is bigger");
-        }
-        else{
-            System.out.println("both are equal");
-        }
+        square(a);
     }
 }
