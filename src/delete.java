144b34da0fee710a22e498d53c8ce4e3409f5f6d
aaaakshatttt <veeakshat4@gmail.com>
2026-03-10 23:29:42 +0530

    i tried

--- src/delete.java
+++ src/delete.java
@@ -3,9 +3,18 @@ public class delete {
     public static void square(int n){
         System.out.println(n*n);
     }
+    public static void eod(int n){
+        if (n % 2 == 0){
+            System.out.println("even");
+        } else if (n % 2 != 0) {
+            System.out.println("odd");
+
+        }
+        else System.out.println("zero");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        square(a);
+        eod(a);
     }
 }
