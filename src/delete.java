1809956549b041767d7f35b686b3931a57f16f45
aaaakshatttt <veeakshat4@gmail.com>
2026-03-10 23:47:46 +0530

    i tried

--- src/delete.java
+++ src/delete.java
@@ -3,10 +3,23 @@ public class delete {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        for (int i = 1;i<=5;i++){
-           for (int j = 1;j<=5;j++){
-               System.out.print("*");
+           if (i == 1 || i == 5){
+               for (int j = 1;j<=5;j++){
+                   System.out.print("* ");
+               }
+               System.out.println();
            }
-           System.out.println();
+           else if (i == 2|| i == 3 || i == 4) {
+               for (int k = 1;k<=5;k++){
+                   if (k == 1 || k == 5){
+                       System.out.print("* ");
+                   } else{
+                       System.out.print("  ");
+                   }
+               }
+               System.out.println();
+
+           }
        }
     }
 }
