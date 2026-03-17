03a00a8a7193c538fea3e334d1cc444d888cf561
aaaakshatttt <veeakshat4@gmail.com>
2026-03-17 08:38:56 +0530

    i tried

---
+++ src/ppap.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ppap {
+    public static void main(String[] args) {
+        int[] arr = {67,98,32,90,44};
+        int sum = 0;
+        for (int i = 0;i<=4;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+
+    }
+}
