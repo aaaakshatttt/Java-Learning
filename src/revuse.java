c22a14920bf79b35ce44d3e09720b916b49357ce
aaaakshatttt <veeakshat4@gmail.com>
2026-04-11 18:09:08 +0530

    i tried

--- src/revuse.java
+++ src/revuse.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 public class revuse {
-public static void sum(int n){
-    int sum = 0;
-    for (int i = 1;i<=n;i++ ){
-        if (i%2!=0){
-            sum = sum+i;
+    public static void evod(int n){
+        if (n == 0){
+            System.out.println("zero");
         }
-
-    } System.out.println(sum);
-}
+        else if (n%2==0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-sum(30);
+evod
     }
 }
