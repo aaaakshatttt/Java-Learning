99f04d1540d24c50387be64f3bc7fabe9948a90c
aaaakshatttt <veeakshat4@gmail.com>
2026-04-11 18:10:28 +0530

    i tried

--- src/revuse.java
+++ src/revuse.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 public class revuse {
     public static void evod(int n){
-        if (n == 0){
-            System.out.println("zero");
-        }
-        else if (n%2==0){
+        if (n%2==0){
             System.out.println("even");
         }
-        else {
-            System.out.println("odd");
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-evod
+
     }
 }
