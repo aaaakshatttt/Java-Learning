fd08d1b555a39e48eee481e06d9e5b35f1131555
aaaakshatttt <veeakshat4@gmail.com>
2026-05-26 17:46:55 +0530

    i tried

--- src/jijij.java
+++ src/jijij.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 public class jijij {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-        System.out.print("enter array size = ");
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-
-
-        for (int i = 0;i<size;i++){
-            System.out.println("enter value of array block " + i);
-            arr[i] = sc.nextInt();
-        }
-
-        for (int i = 0;i<size;i++){
-            System.out.println(arr[i]);
-        }
+       int x = 8;
+       int[] arr = {2,4,8,3,0,5};
+       for (int i = 0;i<6;i++){
+           if (arr[i] == x){
+               System.out.println("mkc found at "+i);
+           }
+       }
     }
 }
--- src/xninputs.java
+++ src/xninputs.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class xninputs {
-    public static void idk(int x, int n){
-        int z = 1;
-        for (int i = 1;i<=n;i++){
-            z = z*x;
-        }
-        System.out.println(z);
-    }
+                        public static void idk(int x, int n){
+                            int z = 1;
+                            for (int i = 1;i<=n;i++){
+                                z = z*x;
+                            }
+                            System.out.println(z);
+                        }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enetr x = ");
