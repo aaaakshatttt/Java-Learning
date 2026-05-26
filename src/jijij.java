98ec7e015d0513dd8ff091e478c95caeef676052
aaaakshatttt <veeakshat4@gmail.com>
2026-05-26 18:09:34 +0530

    i tried

--- src/jijij.java
+++ src/jijij.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 public class jijij {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter dimentiponas");
         int rows = sc.nextInt();
         int columns = sc.nextInt();
         int[][] arr = new int[rows][columns];
 
         for (int i = 0;i<rows;i++){
-            if (i == 0){
-                for (int j = 0;j<columns;j++){
-                    arr[i][j] = sc.nextInt();
-                }
-            } else if (i == 1) {
-                for (int j = 0;j<columns;j++){
-                    arr[i][j] = sc.nextInt();
-                }
-            } else if ( i == 2) {
-                for (int j = 0;j<columns;j++){
-                    arr[i][j] = sc.nextInt();
-
-                }
-
+            for (int j = 0;j<columns;j++){
+                arr[i][j] = sc.nextInt();
             }
+        }
+        for (int i = 0;i<rows;i++){
+            for (int j = 0;j<columns;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
         }
     }
 }
