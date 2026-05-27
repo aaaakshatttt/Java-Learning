ed725ee878f61edb6eaf067227cd001783bffc98
aaaakshatttt <veeakshat4@gmail.com>
2026-05-27 08:13:31 +0530

    i tried

--- src/pract.java
+++ src/pract.java
@@ -3,15 +3,11 @@ public class pract {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = {5,6,3,9,21};
+        int sum = 0;
+        for (int elements : arr){
+            sum = sum + elements;
 
-        System.out.print("enter integer = ");
-        int fuck = sc.nextInt();
-
-        for (int i = 0;i<5;i++){
-            if (arr[i]==fuck){
-                System.out.println("exists at posiiton " + i);
-            }
-            
         }
+        System.out.println(sum);
     }
 }
