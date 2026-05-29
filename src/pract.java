e0b494124ca7752232853a773e8fb118d770c5b8
aaaakshatttt <veeakshat4@gmail.com>
2026-05-29 16:07:58 +0530

    i tried

--- src/pract.java
+++ src/pract.java
@@ -3,8 +3,20 @@ import java.util.*;
 public class pract {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder("Tony");
-        sb.reverse();
-        System.out.println(sb);
+        int[] arr = {7,4,9,2,3,1};
+        int n = arr.length;
+
+        for (int i = 0;i<n-1;i++){
+            for (int j = 0;j<n-i-1;j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];//temp 7
+                    arr[j] = arr[j+1];//4
+                    arr[j+1] = temp;//7 now = 4,7,9,2,3,1a nd lop trnimated now j++ so j = 1
+                }
+            }
+        }
+        for (int k = 0;k<arr.length;k++){
+            System.out.print(arr[k] + " ");
+        }
         }
     }
