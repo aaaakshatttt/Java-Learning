3bbaedb4a42e9a29b807e8cee2069cc5bcd53b83
aaaakshatttt <veeakshat4@gmail.com>
2026-05-30 19:18:20 +0530

    i tried

--- src/pract.java
+++ src/pract.java
@@ -3,20 +3,13 @@ import java.util.*;
 public class pract {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {7,4,9,2,3,1};
-        int n = arr.length;
+        float[] arr = {2.2f,4.5f,6.6f,1,5f,9,54f};
+        int sum = 0;
 
-        for (int i = 0;i<n-1;i++){
-            for (int j = 0;j<n-i-1;j++){
-                if (arr[j] > arr[j+1]){
-                    int temp = arr[j];//temp 7
-                    arr[j] = arr[j+1];//4
-                    arr[j+1] = temp;//7 now = 4,7,9,2,3,1a nd lop trnimated now j++ so j = 1
-                }
-            }
+        for (int i = 0;i<arr.length;i++){
+            sum = sum + arr[i];
         }
-        for (int k = 0;k<arr.length;k++){
-            System.out.print(arr[k] + " ");
-        }
+
+        System.out.println(sum);
         }
     }
