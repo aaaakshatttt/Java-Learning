11997e684ae31f78bc5ea866e97fe3b32c95dd58
aaaakshatttt <veeakshat4@gmail.com>
2026-05-30 19:28:47 +0530

    i tried

--- src/pract.java
+++ src/pract.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class pract {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float[] arr = {2.2f,4.5f,6.6f,1,5f,9.54f};
+        float[] arr = {3,6,19,7,13,45};
+        float n = arr.length;
         float sum = 0;
 
         for (int i = 0;i<arr.length;i++){
             sum = sum + arr[i];
         }
 
-        System.out.println(sum);
+        System.out.println(sum/n);
         }
     }
